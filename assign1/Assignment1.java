be6694d0b99ee8df46095774fbcdfd182fd3459b
Nathan Cazell <ncaz@Nathans-MacBook-Pro.local>
2015-01-20 19:33:14 -0500

    add assign1 to repo

---
+++ assign1/Assignment1.java
@@ -0,0 +1,15 @@
+/*
+Cazell, Nathan
+COP-3252
+Assignment 1
+1/12/15
+*/
+
+public class Assignment1
+{
+  // My main method
+  public static void main(String[] args)
+  {
+    System.out.println("Welcome to COP-3252 Java Programming!");
+  }
+}
